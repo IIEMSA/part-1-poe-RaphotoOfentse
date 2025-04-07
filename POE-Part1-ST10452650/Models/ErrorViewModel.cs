a547c6e3b7755735466ff0115c85f20d79dfefe9
Pabatso-Raphoto <ST10452650@imconnect.edu.za>
2025-04-07 15:27:15 +0200

    Part 1 submission

---
+++ POE-Part1-ST10452650/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace POE_Part1_ST10452650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
